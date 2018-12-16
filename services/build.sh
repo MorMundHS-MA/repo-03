442db3519d244c82886c3c2b2a05f58d5e6c5745
moritz <1611229@stud.hs-mannheim.de>
2018-12-16 16:59:21 +0100

    Added maven multi module pom

--- services/build.sh
+++ services/build.sh
@@ -1,6 +1,2 @@
 #!/bin/bash
-git pull
-(cd common/; mvn clean compile install)
-(cd login-server/; mvn clean compile test install assembly:single && sudo docker build -t loginserver .)
-(cd chat-server/; mvn clean compile test install assembly:single && sudo docker build -t chatserver .)
-(cd register-server/; mvn clean compile test install assembly:single && sudo docker build -t registerserver .)
+mvn clean package
